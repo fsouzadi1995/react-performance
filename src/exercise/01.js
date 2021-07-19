f976fd25a906f6e3cc6ac95c45ecb18df847cd98
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-07-18 21:45:04 -0300

    chore: initial commit & exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,19 +2,18 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+
+// Dynamic import, webpack caches the result and makes it a one time only fetch
+// Name chunk
+const loadGlobe = () => import(/* webpackChunkName: "globe" */'../globe')
+
+const Globe = React.lazy(loadGlobe);
+// import Globe from '../globe';
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
@@ -26,22 +25,27 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+
+      {/* Dynamically load onFocus and onMouseEnter */}
+      <label style={{marginBottom: '1rem'}}  onFocus={loadGlobe} onMouseEnter={loadGlobe}>
         <input
           type="checkbox"
           checked={showGlobe}
           onChange={e => setShowGlobe(e.target.checked)}
+          
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
-      </div>
+
+      {/* Fallback for asynchronous loading */}
+      <React.Suspense fallback={<div>Loading..</div>}>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
+      </React.Suspense>
+     
     </div>
   )
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
 export default App
