a5089dbba535b041df7ddb9b126e6ff1e2d97f93
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-07-20 21:57:53 -0300

    chore: exercise 02 - add webworker for expensive operations

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,7 +3,8 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
+import {getItems} from '../workerized-filter-cities'
+import {useAsync} from '../utils'
 import {useForceRerender} from '../utils'
 
 function Menu({
@@ -59,11 +60,16 @@ function ListItem({
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
+  const {data: allItems, run} = useAsync()
+
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
 
   // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = React.useMemo(() => {
-    return getItems(inputValue)
-  }, [inputValue])
+  // const allItems = React.useMemo(() => {
+  //   return getItems(inputValue)
+  // }, [inputValue])
   const items = allItems.slice(0, 100)
 
   const {
